f8ac726c6b9941508286c3be10703cf7d09d8c77
GUO <1174700652@QQ.COM>
2021-10-23 22:24:50 +0800

    写的拉跨了

---
+++ dist/index.65c7a1e5.js
@@ -0,0 +1,12 @@
+const div = dom.create("    <tr><td>2</td></tr>");
+console.log(div);
+const testDiv = dom.find('#test')[0];
+console.log(testDiv);
+console.log(dom.style(div, 'color', 'red'));
+const divList = dom.find('.red');
+console.log(divList);
+const t = dom.find('#travel')[0];
+dom.each(dom.children(t), (n)=>dom.style(n, 'color', 'red')
+);
+
+//# sourceMappingURL=index.65c7a1e5.js.map
